8128fc87dcc08a0aeba185fa08e6c9eb956a830a
“domekenshi” <kfprog5136@gmail.com>
2024-07-24 23:15:58 +0900

    趣味、居住地追加

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.example.jetprofile
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -83,20 +88,8 @@ class MainActivity : ComponentActivity() {
                             // スペース
                             Spacer(modifier = Modifier.height(20.dp))
                             // Email
-                            Row(verticalAlignment = Alignment.CenterVertically){
-                                Icon(
-                                    imageVector = Icons.Default.Email,
-                                    contentDescription = "email"
-                                )
-                                // スペース
-                                Spacer(modifier = Modifier.width(10.dp))
-                                Text(
-                                    text = "Email",
-                                    color = Color.Gray,
-                                    fontSize = 14.sp,
-                                    fontWeight = FontWeight.Bold
-                                )
-                            }
+                            label(icon = Icons.Default.Email, text = "Email")
+
                             // スペース
                             Spacer(modifier = Modifier.height(10.dp))
                             Text(
@@ -123,12 +116,52 @@ class MainActivity : ComponentActivity() {
                                     color=Color.White,
                                 )
                             }
+                            // スペース
+                            Spacer(modifier = Modifier.height(20.dp))
+                            // 趣味と居住地
+                            Column (
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clip(RoundedCornerShape(10.dp))
+                                    .background(
+                                        Color.LightGray.copy(alpha = 0.3f)
+                                    )
+                                    .padding(horizontal = 10.dp, vertical = 20.dp)
+                                    ){
+                                label(icon = Icons.Default.Favorite,
+                                    text = "キックボクシング",
+                                    color = Color.Gray)
+                                // スペース
+                                Spacer(modifier = Modifier.height(20.dp))
+                                label(icon = Icons.Default.LocationOn,
+                                    text = "東京都大田区",
+                                    color = Color.Gray)
+
+                            }
+
+
 
                         }
-
                     }
                 }
             }
         }
+    }
+}
+@Composable
+fun label(icon: ImageVector,text:String,color:Color = MaterialTheme.colors.onBackground){
+    Row(verticalAlignment = Alignment.CenterVertically){
+        Icon(
+            imageVector = icon,
+            contentDescription = null
+        )
+        // スペース
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            text = text,
+            color = color,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
